6a1b66603c69645cbd8b98a30fcf589c78d897ee
amana k <amanak@amanas-Mac-mini.local>
2025-09-25 23:34:25 +0100

    Fixing build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1894ff90e9aa9610f77a46f747dd9d85aa01d4b1fd3b0889a569c010760a5361
-size 533
+oid sha256:7ce786df0a8e0bc3f04bc8dfe90009e82cf9b7cf9d700a560e5bbb8a422be5c6
+size 560
